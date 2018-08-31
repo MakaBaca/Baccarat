68395a57adfc9fc49d60ae564b9b4a94c28fcd33
MakaBaca <42783016+MakaBaca@users.noreply.github.com>
2018-08-31 15:04:17 +0530

    Create ShoeRepo.java

---
+++ boot/src/main/java/com/mak/baca/dao/ShoeRepo.java
@@ -0,0 +1,9 @@
+package com.mak.baca.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mak.baca.model.Shoe;
+
+public interface ShoeRepo extends CrudRepository<Shoe, Integer> {
+
+}
