945d2187f12e2c6e84ce0ad9c3e3653a4bc99b18
MakaBaca <42783016+MakaBaca@users.noreply.github.com>
2018-08-31 15:04:37 +0530

    Create PlayerRepo.java

---
+++ boot/src/main/java/com/mak/baca/dao/PlayerRepo.java
@@ -0,0 +1,9 @@
+package com.mak.baca.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mak.baca.model.Player;
+
+public interface PlayerRepo extends CrudRepository<Player, Integer>{
+
+}
